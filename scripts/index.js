9456d29c340bb385723e66e552b04d214286e469
gerard415 <chinedumike85@gmail.com>
2024-07-19 14:43:55 -0400

    project 4 final stage

--- scripts/index.js
+++ scripts/index.js
@@ -28,11 +28,54 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalCloseButton = document.querySelector("#modal-close-button");
-
-profileEditButton.addEventListener("click", () =>
-  profileEditModal.classList.add("modal__opened")
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
 );
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+const closePopUp = () => {
+  profileEditModal.classList.remove("modal__opened");
+};
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+
+  profileEditModal.classList.add("modal__opened");
+});
 
 modalCloseButton.addEventListener("click", () =>
   profileEditModal.classList.remove("modal__opened")
 );
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopUp();
+});
+
+const getCardElement = (cardData) => {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+
+  return cardElement;
+};
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+
+  cardListEl.prepend(cardElement);
+});
