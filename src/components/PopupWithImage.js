bbf8d193055b642b71846172f7307215f2a45e08
gerard415 <chinedumike85@gmail.com>
2024-09-12 15:46:57 -0400

    PopupWithImage class

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,25 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
+  constructor(popUpSelector) {
+    super(popUpSelector);
+    this._popUpElement = document.querySelector(popUpSelector);
+    this._popUpImage = this._popUpElement.querySelector(
+      ".modal__preview-image"
+    );
+    this._popUpTitle = this._popUpElement.querySelector(
+      ".modal__preview-image-heading"
+    );
+  }
+
+  open({ name, link }) {
+    // set the image's src and alt
+    this._popUpImage.src = link;
+    this._popUpImage.alt = name;
+
+    // set the caption's textContent
+    this._popUpTitle.textContent = name;
+
+    super.open();
+  }
+}
